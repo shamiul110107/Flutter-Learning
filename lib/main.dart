d1e665dc77497f8005cabe61b2c425762eaa1cbc
shamiul <shamiulcse.pust@gmail.com>
2024-04-29 12:08:54 +0700

    Use list view to show full list of data

--- lib/main.dart
+++ lib/main.dart
@@ -30,76 +30,62 @@ class MyApp extends StatelessWidget {
                 icon: const Icon(Icons.logout))
           ],
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(20)),
-                padding: const EdgeInsets.all(20),
-                child: const Column(
-                  children: [
-                    Text(
-                      "My First Widget",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                          backgroundColor: Colors.transparent),
-                    ),
-                    SizedBox(height: 20),
-                    Icon(Icons.favorite, color: Colors.red, size: 50)
-                  ],
-                ),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.green, borderRadius: BorderRadius.circular(20)),
-                padding: const EdgeInsets.all(20),
-                child: const Column(
-                  children: [
-                    Text(
-                      "My First Widget",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                          backgroundColor: Colors.transparent),
-                    ),
-                    SizedBox(height: 20),
-                    Icon(Icons.favorite, color: Colors.red, size: 50)
-                  ],
-                ),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(20)),
-                padding: const EdgeInsets.all(20),
-                child: const Column(
-                  children: [
-                    Text(
-                      "My First Widget",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                          backgroundColor: Colors.transparent),
-                    ),
-                    SizedBox(height: 20),
-                    Icon(Icons.favorite, color: Colors.red, size: 50)
-                  ],
-                ),
-              ),
-            ),
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          children: const [
+            BoxWidget(text: "Fav View", icon: Icons.favorite, iconColor: Colors.red),
+            BoxWidget(text: "Star View", icon: Icons.star, iconColor: Colors.orange),
+            BoxWidget(text: "Sunny View", icon: Icons.sunny, iconColor: Colors.white),
+            BoxWidget(text: "Football View", icon: Icons.sports_football, iconColor: Colors.red),
+            BoxWidget(text: "Cricket View", icon: Icons.sports_cricket, iconColor: Colors.orange),
+            BoxWidget(text: "Sunny View", icon: Icons.sports_baseball, iconColor: Colors.white),
+            BoxWidget(text: "Fav View", icon: Icons.favorite, iconColor: Colors.red),
+            BoxWidget(text: "Star View", icon: Icons.star, iconColor: Colors.orange),
+            BoxWidget(text: "Sunny View", icon: Icons.sunny, iconColor: Colors.white)
           ],
         ),
+      ),
+    );
+  }
+}
+
+class BoxWidget extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  final Color iconColor;
+
+  const BoxWidget({
+    super.key,
+    required this.text,
+    required this.icon,
+    required this.iconColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.green,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        children: [
+          Text(
+            text,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Icon(
+            icon,
+            color: iconColor,
+            size: 50,
+          ),
+        ],
       ),
     );
   }
